a7b28871f44d320d9413d15f018bea24a62a38c8
QasimTariq978 <88278333+QasimTariq978@users.noreply.github.com>
2022-02-08 21:49:48 +0500

    Update settings.py

--- gym/gym/settings.py
+++ gym/gym/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g^)#bt=j5+(btq$h_bgz1de1w+7d7e-8uc*+j8u7jg^e&g!l8r'
+SECRET_KEY = *REMOVED*
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
